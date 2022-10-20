75ccca78ffdf8051e6b79c16f49594356c05e582
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2022-10-20 15:46:57 -0300

    sistema do cronômetro finalizado

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let horas = 0
 let minutos = 0
 let segundos = 0
 
-document.querySelector('#horas-estudando').value = horas
-document.querySelector('#minutos-estudando').value = minutos
-document.querySelector('#segundos-estudando').value = segundos
+document.querySelector('#horas-estudando').value = `0${horas}`
+document.querySelector('#minutos-estudando').value = `0${minutos}`
+document.querySelector('#segundos-estudando').value = `0${segundos}`
 
 // Intervalo do cronômetro
 let intervalo
@@ -20,37 +20,33 @@ inputMinutosEstudando.addEventListener('focusout', () => verificarValorMS(inputM
 inputSegundosEstudando.addEventListener('focusout', () => verificarValorMS(inputSegundosEstudando))
 
 // Verificações de valores
+
+// minutos e segundos
 function verificarValorMS (input) {
   let valor = Number(input.value)
+  let length = input.value.length
+
   if (valor < 0 || valor > 59) {
     alert('Digite valores entre 00 e 59')
     input.value = '00'
   }
 
-  if (valor < 10) {
+  if (valor < 10 && length < 2) {
     input.value = `0${input.value}`
   }
 }
 
+// horas
 function verificarValorH (input) {
   let valor = Number(input.value)
+  let length = input.value.length
 
-  if (valor < 10) {
+  if (valor < 10 && length < 2) {
     input.value = `0${input.value}`
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
+// Botão que ativa o cronômetro
 const btnComecarEstudando = document.querySelector('#comecar-estudando')
 btnComecarEstudando.addEventListener('click', cronometro)
 
@@ -58,30 +54,11 @@ function cronometro () {
   intervalo = setInterval(decremento, 1000)
 }
 
-
-
-
-
-
-
-
+// Função de decremento do cronômetro
 function decremento () {
   let horasEstudando = document.querySelector('#horas-estudando').value
   let minutosEstudando = document.querySelector('#minutos-estudando').value
   let segundosEstudando = document.querySelector('#segundos-estudando').value
-
-  if (segundosEstudando === '00') {
-    clearInterval(intervalo)
-    return
-  }
-  if (minutosEstudando === '00') {
-    clearInterval(intervalo)
-    return
-  }
-  if (horasEstudando === '00') {
-    clearInterval(intervalo)
-    return
-  }
 
   let horasEstudandoNumero = Number(horasEstudando)
   let minutosEstudandoNumero = Number(minutosEstudando)
@@ -90,6 +67,7 @@ function decremento () {
   document.querySelector('#segundos-estudando').value = segundosEstudandoNumero
   document.querySelector('#minutos-estudando').value = minutosEstudandoNumero
   document.querySelector('#horas-estudando').value = horasEstudandoNumero
+
 
   if (horasEstudandoNumero == 0 && minutosEstudandoNumero == 0 && segundosEstudandoNumero <= 0) {
     clearInterval(intervalo)
@@ -106,7 +84,22 @@ function decremento () {
     minutosEstudandoNumero = 59
   }
 
-  document.querySelector('#segundos-estudando').value = segundosEstudandoNumero
-  document.querySelector('#minutos-estudando').value = minutosEstudandoNumero
-  document.querySelector('#horas-estudando').value = horasEstudandoNumero
+
+  if (segundosEstudandoNumero < 10) {
+    document.querySelector('#segundos-estudando').value = `0${segundosEstudandoNumero}`
+  } else {
+    document.querySelector('#segundos-estudando').value = segundosEstudandoNumero
+  }
+
+  if (minutosEstudandoNumero < 10) {
+    document.querySelector('#minutos-estudando').value = `0${minutosEstudandoNumero}`
+  } else {
+    document.querySelector('#minutos-estudando').value = minutosEstudandoNumero
+  }
+
+  if (horasEstudandoNumero < 10) {
+    document.querySelector('#horas-estudando').value = `0${horasEstudandoNumero}`
+  } else {
+    document.querySelector('#horas-estudando').value = horasEstudandoNumero
+  } 
 }
